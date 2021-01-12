de2d51dba37111228a7b9f79772138b0728e2bac
Akshat Rana <65679227+Akshat511716@users.noreply.github.com>
2021-01-12 22:22:39 +0530

    Add files via upload

---
+++ Senti.py
@@ -0,0 +1,23 @@
+from textblob import TextBlob
+
+x = input("Hi! I recently gave a public session and found that were one of the attendees! How was the session for you? ")
+y = TextBlob(x)
+z = y.sentiment.polarity
+
+if z<0:
+    x1 = input("Awesome! Would you like to give in more details about your experience in person!")
+    y1 = TextBlob(x1)
+    z1 = y1.sentiment.polarity
+
+    if z1<0:
+        print("Thank you for the feedback")
+    elif z1==0:
+        print("Great! Awesome feedback")
+    elif z1>0 and z1<=1:
+        print("Great! Thank you. We can connect over here!")
+
+elif z==0:
+    print("Oh! Thank you for attending!")
+
+elif z>0 and z<=1:
+    print("Great! Thank you so much for your valuable words!")
